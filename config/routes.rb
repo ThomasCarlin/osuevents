e95d7dcf236a9f1ea62d5765e2d4de514995d8f8
ThomasCarlin <thomasjeffersoncarlin@gmail.com>
2015-07-25 16:16:59 -0400

    new layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   get 'visitors/addcalendar'
+  get 'visitors/test'
   post 'visitors/addcalendar'
   devise_for :users
   resources :users
